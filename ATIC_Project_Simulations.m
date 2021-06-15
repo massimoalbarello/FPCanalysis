ee7f439c03cc94ba6ed46853dd2d64994e796bfa
andreadacol98 <adacol@student.ethz.ch>
2021-06-15 18:00:05 +0100

    Cloop control

--- ATIC_Project_Simulations.m
+++ ATIC_Project_Simulations.m
@@ -4,7 +4,9 @@ n = 50;
 % Defining the number of nodes to be sampled
 p = 5;
 %Fix simulation length
-t_end = 50;               
+t_end = 500;       
+%plotting variable
+plotting = false;
 
 % Defining the initial conditions on the opinions
 x_0 = zeros(n , 1);
@@ -53,8 +55,11 @@ end
 
 %Plotting the DTime evolution of all the states (Comparing with average
 %matrix)
-figure(1); plot(0:1:t_end , x_k , 'LineWidth' , 1.3); grid on;
-figure(2); plot(0:1:t_end , x_k_expected, 'LineWidth' , 2); grid on;
+
+if(plotting == true)
+    figure(1); plot(0:1:t_end , x_k , 'LineWidth' , 1.1); grid on;
+    figure(2); plot(0:1:t_end , x_k_expected, 'LineWidth' , 2); grid on;
+end
 
 % Computing the Laplacian matrix associated to the sequence of A(k)
 L_sequence = zeros(n , n , t_end);
@@ -76,7 +81,11 @@ for i = 1:t_end
     end
     x_init = x_t(: , 100*(i - 1)+ k + 1);
 end
-figure(3); plot(t_span , x_t, 'LineWidth' , 1.3); grid on;
+
+if(plotting == true)
+    figure(3); plot(t_span , x_t( : ,:), 'LineWidth' , 1.1); hold on; grid on;
+    % plot(0:1:t_end , x_k(1:2 ,:), 'LineWidth' , 1.3)
+end
 
 %Computing the solution for the continuous time system (Expected L)
 x_t_expected = zeros(n , length(t_span));
@@ -86,4 +95,70 @@ end
 
 
 %Plotting the state evolution in continuous time (for expected L)
-figure(4); plot(t_span , x_t_expected, 'LineWidth' , 2); grid on;
+if(plotting == true)
+    figure(4); plot(t_span , x_t_expected, 'LineWidth' , 2); grid on;
+end
+
+
+
+%% IMPLEMENTING THE FEEDBACK CONTROL LAW
+
+%% STEP 1
+% Assumptions:
+% - One selfish agent IOTA NODE
+% - Randomized Adjacency
+% - Myopic Selfish Agent C not strictly positive C = [c_1 , ... , c_n] only
+%   m c_i's are non-zero, with m<n
+% - complete undelying network
+% - The mean has to converge to a reference ref
+
+n_selfish = 1;
+
+%Define reference value
+ref=mean(x_0);
+
+% Define the closed loop system state space representation (A , B , C , D)
+B = [ones(n_selfish , 1) ; zeros(n - n_selfish , 1)];
+C = (1/n)*[ones(1 , n)];
+Kp = 10;
+Ki = 0.01;
+
+%Initial condition for the augmented states
+x_0=[x_0;0];
+x_k_cloop = zeros(n + n_selfish , t_end+1);
+x_k_cloop(: , 1) = x_0;
+
+A_cl_sequence = zeros(n + n_selfish , n + n_selfish , t_end);
+for k  = 1:t_end
+    A_cl_sequence(: , : , k) = [A_sequence(: , : , k)-B*Kp*C , B*Ki ; -C , 1];
+    x_k_cloop(: , k+1) = A_cl_sequence(: , : , k) * x_k_cloop(: , k) + [B*Kp ; 1]*ref;
+end
+figure(5) ; plot(0:1:t_end , mean(x_k_cloop,1) ,  'LineWidth' , 2); grid on; hold on; legend();
+plot(0:1:t_end , x_k_cloop(51 , :) ,  'LineWidth' , 2); hold off;
+
+
+%% STEP 2
+% Assumptions:
+% - One selfish agent IOTA NODE
+% - Randomized Adjacency
+% - Myopic Selfish Agent C not strictly positive C = [c_1 , ... , c_n] only
+%   m c_i's are non-zero, with m<n
+% - complete undelying network
+% - The error wrt the other nodes has to converge to a reference ref = 0
+
+% Define the closed loop system state space representation (A , B , C , D)
+B = [ones(n_selfish , 1) ; zeros(n - n_selfish , 1)];
+C = [n-n_selfish , - ones(1 , n-n_selfish)];
+Kp = 0.01;
+Ki = 0.001;
+
+% Defgining new reference
+ref = 0.001;
+
+A_cl_sequence = zeros(n + n_selfish , n + n_selfish , t_end);
+for k  = 1:t_end
+    A_cl_sequence(: , : , k) = [A_sequence(: , : , k)-B*Kp*C , B*Ki ; -C , 1];
+    x_k_cloop(: , k+1) = A_cl_sequence(: , : , k) * x_k_cloop(: , k) + [B*Kp ; 1]*ref;
+end
+figure(6) ; plot(0:1:t_end , x_k_cloop(:,:) ,  'LineWidth' , 2); grid on; hold on; legend();
+plot(0:1:t_end , x_k_cloop(51 , :) ,  'LineWidth' , 2); hold off;
