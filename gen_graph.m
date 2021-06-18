8cffbdb6bf62f1aac7015b1c13090ec56fb9bf5e
andreadacol98 <adacol@student.ethz.ch>
2021-06-18 14:53:49 +0100

    SEMPRE LO STESSO DI PRIMA

--- gen_graph.m
+++ gen_graph.m
@@ -1,4 +1,4 @@
-function [topology] = gen_graph(n,p)
+function [topology , sum_row , Irreducibility] = gen_graph(n,p)
 %Generate underlying topology like:
 % - self loops
 % - at least p+1 out neighbours, including himself
@@ -14,31 +14,6 @@ for i=1:n
         topology(i,i) =1;
     end
 end
-
-%Checking irreducibility of topology binary matrix
-Irreducibility = eye(n);
-for i = 1:(n-1)
-    Irreducibility = Irreducibility + topology^i;
-end
-
-% Checking the number of out neighbours including i itself, and irreducibility, if not good,
-% we call the function again in recursive fashion
-sum_row = sum(topology , 2);
-if (sum_row < (p+1)*ones(n,1) & Irreducibility > zeros(n))
-    fprintf('Need more neighbours, Irreducible\n');
-    topology = gen_graph(n,p);
-elseif (sum_row < (p+1)*ones(n,1) & Irreducibility <= zeros(n))
-    fprintf('Need more neighbours, Not Irreducible\n');
-    topology = gen_graph(n,p);
-elseif (sum_row >= (p+1)*ones(n,1) & Irreducibility <= zeros(n))
-    fprintf('Enough neighbours, Not Irreducible\n');
-    topology = gen_graph(n,p);
-elseif (sum_row >= (p+1)*ones(n,1) & Irreducibility > zeros(n))
-    fprintf('Enough neighbours, Irreducible\n');
-% else
-%     fprintf('I forgot one case\n');
-end
-
    
 end
 
